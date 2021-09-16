1f046a14fdbc400aa58d07d2bf40580a23eef84e
Srinath <srinath.a@techjays.com>
2021-09-16 19:04:56 +0530

    Initial commit

---
+++ Mylib/src/main/java/com/techjays/mylib/Toasty.kt
@@ -0,0 +1,12 @@
+package com.techjays.mylib
+
+import android.content.Context
+import android.widget.Toast
+
+class Toasty {
+    companion object {
+        fun toaster(context: Context, msg: String) {
+            Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+        }
+    }
+}
---
+++ app/src/main/java/com/techjays/chatapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.techjays.chatapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.techjays.mylib.Toasty
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
